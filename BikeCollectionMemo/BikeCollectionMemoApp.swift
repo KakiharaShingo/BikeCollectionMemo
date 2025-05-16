f0620b621829687f065725e1cdf70b7324f412eb
KakiharaShingo <sk.shingo.10@gmail.com>
2025-05-17 00:33:32 +0900

    Initial Commit

---
+++ BikeCollectionMemo/BikeCollectionMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeCollectionMemoApp.swift
+//  BikeCollectionMemo
+//
+//  Created by 垣原親伍 on 2025/05/17.
+//
+
+import SwiftUI
+
+@main
+struct BikeCollectionMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BikeCollectionMemoTests/BikeCollectionMemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  BikeCollectionMemoTests.swift
+//  BikeCollectionMemoTests
+//
+//  Created by 垣原親伍 on 2025/05/17.
+//
+
+import Testing
+@testable import BikeCollectionMemo
+
+struct BikeCollectionMemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
